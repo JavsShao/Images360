84e01f07d60eb4ccfc211cfa3a88f3ca1c18717c
hamdi <javs_shao@163.com>
2018-12-17 16:08:07 +0800

    Mongodb数据库初始化

--- images360/images360/pipelines.py
+++ images360/images360/pipelines.py
@@ -9,3 +9,13 @@
 class Images360Pipeline(object):
     def process_item(self, item, spider):
         return item
+
+class MongoPipeline(object):
+    def __init__(self, mongo_url, mongo_db):
+        '''
+        Mongodb数据库初始化
+        :param mongo_url: 地址
+        :param mongo_db: 数据库
+        '''
+        self.mongo_url = mongo_url
+        self.mongo_db = mongo_db
