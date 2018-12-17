a84f47c8e7cbe2e1050c910a4ca761b7ce4bba05
hamdi <javs_shao@163.com>
2018-12-17 16:27:29 +0800

    Mongodb配置

--- images360/images360/pipelines.py
+++ images360/images360/pipelines.py
@@ -4,6 +4,12 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+import pymysql
+
+from scrapy import Request
+from scrapy.exceptions import DropItem
+from scrapy.pipelines.images import ImagesPipeline
 
 
 class Images360Pipeline(object):
@@ -19,3 +25,27 @@ class MongoPipeline(object):
         '''
         self.mongo_url = mongo_url
         self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        '''
+
+        :param crawler:
+        :return:
+        '''
+        return cls(
+            mongo_url=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DB')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_url)
+        self.db = self.client[self.mongo_db]
+
+    def process_item(self, item, spider):
+        name = item.collection
+        self.db[name].insert(dict[item])
+        return item
+
+    def close_spider(self, spider):
+        self.client.close()
